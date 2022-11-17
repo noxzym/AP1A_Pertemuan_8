a25637ded1f5bbe7e7642fdcf3ce65d58e9968ea
noxzym <orchitiadi@gmail.com>
2022-11-17 10:40:27 +0700

    chore(matriks): add isi_matriks_4x5.py

---
+++ matriks/isi_matriks_4x5.py
@@ -0,0 +1,11 @@
+from numpy import array
+
+baris = 4
+kolom = 5
+matriks = array([array([0]*5)]*4)
+
+for brs in range(baris):
+    for kol in range(kolom):
+        matriks[brs][kol] = int(input(f"elemen baris = {brs} kolom = {kol}: "))
+
+print(matriks)
