5addf89bfcd84fb99bf0395966abfa9ece358c52
noxzym <orchitiadi@gmail.com>
2022-11-17 10:40:10 +0700

    chore(larik): add sort_larik.py

---
+++ larik/sort_larik.py
@@ -0,0 +1,13 @@
+def Sort_Larik(A: list[int], N: int):
+    temp = 0
+    for idx1 in range(N-1, 1):
+        for idx2 in range(idx1, N, 1):
+            if A[idx1] > A[idx2]:
+                temp = A[idx1]
+                A[idx1] = A[idx2]
+                A[idx2] = temp
+
+A = [9,1, 6,10,4,2,3,5,8,7]
+N = len(A)
+Sort_Larik(A, N)
+print(A)
