80b2719fc704c67e315a775985bc7c67792da6ba
noxzym <orchitiadi@gmail.com>
2022-11-17 18:24:42 +0700

    chore(larik/cari_larik): menambahkan output indikator

--- larik/cari_larik.py
+++ larik/cari_larik.py
@@ -1,13 +1,17 @@
-def Cari_elemen(A: list[int], N: int, x: int) -> int:
+def Cari_elemen(A: list[int], N: int, x: int) -> str:
     indikator = "N"
     posisi = 0
     indeks = 1
-    while indeks < N + 1 and indikator == "N":
-        if A[indeks] == x:
-            indikator = "Y"
-            posisi = indeks
-        indeks = indeks + 1
+    
+    try:
+        while indeks < N + 1 and indikator == "N":
+            if A[indeks] == x:
+                indikator = "Y"
+                posisi = indeks
+            indeks += 1
+    except IndexError:
+        pass
 
-    return posisi
+    return f"Indikator = {indikator}, Posisi = {posisi if indikator == 'Y' else 'Tidak ada'}"
 
-print(Cari_elemen([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 10, 7))
+print(Cari_elemen([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 10, 11))
